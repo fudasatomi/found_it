860314e8bfe0da8d1abc7560d3eabce797686236
fudasatomi <fudasatomi.dmm@gmail.com>
2020-04-07 20:41:52 +0900

    device login

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,30 @@
 Rails.application.routes.draw do
-  get 'recipes/new'
-  get 'recipes/index'
-  get 'recipes/category_index'
-  get 'recipes/show'
-  get 'recipes/edit'
-  get 'relationships/follow'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/leave'
-  get 'homes/top'
-  get 'homes/about'
   devise_for :users
+
+  root 'homes#top'
+  get 'homes/about'
+
+  resources :users, only: [:index, :show ,:edit ,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'follows' => 'relationships#follower', as: 'follows'
+    get 'followers' => 'relationships#followed', as: 'followers'
+   collection do
+     get 'leave'
+     get 'follow'
+   end
+   member do
+     get 'favorite'
+   end
+   end
+
+   resources :recipes do
+    resource :favorites, only:[:create,:destroy]
+    resource :book_comments, only:[:create, :destroy]
+    collection do
+      get 'category_index'
+    end
+  end
+
+    get 'search' => 'search#search', as: 'search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
