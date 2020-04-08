9c64629f64dcbe49894dd431bcc2e47bb44c95ca
fudasatomi <fudasatomi.dmm@gmail.com>
2020-04-08 19:33:37 +0900

    モデル修正、seedファイルにカテゴリー作成

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
-class Categorie < ApplicationRecord
+class Category < ApplicationRecord
 
 	has_many :recipes
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_045649) do
+ActiveRecord::Schema.define(version: 2020_04_08_103106) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
