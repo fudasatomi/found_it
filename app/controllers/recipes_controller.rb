6e60999ad8f7cc5f2f88caf45299779f224904b7
fudasatomi <fudasatomi.dmm@gmail.com>
2020-04-08 17:06:20 +0900

    nested_form_field gem install

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,18 @@
 class RecipesController < ApplicationController
   def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params) #recipeモデルのテーブルを使用しているのでrecipeコントローラで保存する。
+    @recipe.user_id = current_user.id
+    # if
+    @recipe.save #入力されたデータをdbに保存する。
+    redirect_to @recipe, notice: "successfully created recipe!"#保存された場合の移動先を指定。
+    # else
+      # @recipes = recipe.all
+      # render 'index'
+    # end
   end
 
   def index
@@ -13,4 +26,10 @@ class RecipesController < ApplicationController
 
   def edit
   end
+
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:title,:outline, :image, :category_id, :is_closed )
+    end
 end
