4c61635e7588c9c46c0a8d9d33d15aa78fe21eb3
fudasatomi <fudasatomi.dmm@gmail.com>
2020-04-20 00:14:35 +0900

    headerに検索フォーム作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+	before_action :set_search
+	  def set_search
+	    @q        = Recipe.where(is_closed: false).search(params[:q])
+	  end
+
   protected
 
 	  def configure_permitted_parameters
