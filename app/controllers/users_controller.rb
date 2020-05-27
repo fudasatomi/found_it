a91a49e188271fd563014d64a80e79857d18d98c
fudasatomi <fudasatomi.dmm@gmail.com>
2020-05-27 13:53:35 +0900

    Revert "コンフリクト修正"
    This reverts commit 6d7d7c1c80944e9a4f040503133497cb04db72b6.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
   def recipes
   	@user = User.find(params[:id])
-    if @user == current_user
-    	@recipes = Recipe.where(user_id: @user.id).page(params[:page]).reverse_order
-    else
-      @recipes = Recipe.where(user_id: @user.id,is_closed: false).page(params[:page]).reverse_order
-    end
+  	@recipes = Recipe.where(user_id: @user.id)
   end
 
   def show
